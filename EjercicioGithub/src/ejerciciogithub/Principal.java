d6a6bc3dba77056b10a4eb16811f49eaaa8c261a
haitan3 <59873675+haitan3@users.noreply.github.com>
2020-01-21 19:02:13 +0100

    Opcion1 TERMINADA
    Ahora falta ponerse en las dos restantes,

--- EjercicioGithub/src/ejerciciogithub/Principal.java
+++ EjercicioGithub/src/ejerciciogithub/Principal.java
@@ -27,35 +27,52 @@ insertar y se tiene que visualizar los datos del empleado que más gana. Si hay 
 empleados que coinciden, se visualizará los datos del último empleado (qué gana más*/
     public static void main(String[] args) {
 
-        String nombre;
-        int edad = 0, contador = 1;
-        float sueldo;
+        String nombre, nombreVisualizar = null;
+        int edad = 0, contador = 1, edad1 = 0;
+        float sueldo, sueldoMayor = Integer.MIN_VALUE;
         boolean insertarLetras = false;
 
         while (!insertarLetras) {
             System.out.println("\t\t\nVamos a pedir los datos del empleado número " + contador + " :");
             nombre = pedirDatos.PedirDatosTeclado.pedirString("\n\tIntroduzca el nombre del empleado/a: ");
             contador++;
-           // edad = pedirDatos.PedirDatosTeclado.pedirInt("\n\tIntroduzca la edad del empleado/a: ");
-            while (edad < 18 || edad > 65) {
+            // edad = pedirDatos.PedirDatosTeclado.pedirInt("\n\tIntroduzca la edad del empleado/a: ");
+
+            do {
 
                 try {
                     edad = pedirDatos.PedirDatosTeclado.pedirInt("\n\tIntroduzca la edad del empleado/a: ");
 
                 } catch (NumberFormatException errorletras) {
                     System.out.println("\n\t\t" + Colores.RED + "Valor no admitido, no inserte letras.");
+                    edad = pedirDatos.PedirDatosTeclado.pedirInt("\n\tIntroduzca la edad del empleado/a: ");
                 } catch (InputMismatchException error) {
                     System.out.println("\n\t\t" + Colores.RED + "Valor no admitido, no inserte letras.");
+                    edad = pedirDatos.PedirDatosTeclado.pedirInt("\n\tIntroduzca la edad del empleado/a: ");
                 }
-            }
+            } while (edad < 18 || edad > 65);
             try {
 
                 sueldo = pedirDatos.PedirDatosTeclado.pedirFloat("\n\tIntroduzca el sueldo mensual del empleado/a: ");
+                if (sueldo > sueldoMayor) {
+
+                    sueldoMayor = sueldo;
+                    edad1 = edad;
+                    nombreVisualizar = nombre;
+                }
             } catch (NumberFormatException errorletras) {
+                System.out.println("\n\t\t" + Colores.RED + "Se han insertado letras. salimos de la opción.");
+                insertarLetras = true;
+            } catch (InputMismatchException error) {
                 System.out.println("\n\t\t" + Colores.RED + "Se han insertado letras. salimos de la opción.");
                 insertarLetras = true;
             }
         }
+        System.out.println("\n\t" + Colores.GREEN + "Se han insertado un total de " + contador + " empleados/as.");
+        System.out.println("El empleado con el mayor sueldo es: ");
+        System.out.println("\n\t" + Colores.BLUE + "Nombre: " + nombreVisualizar);
+        System.out.println("\n\t" + Colores.BLUE + "Edad: " + edad1);
+        System.out.println("\n\t" + Colores.BLUE + "Sueldo: " + sueldoMayor);
 
     }
 
